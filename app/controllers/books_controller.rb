9e70ea0f4e3fdd79862b616230322104ccda2704
behrami <behram1993@gmail.com>
2018-05-08 00:02:51 -0400

    done with html and text

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,14 @@
 class BooksController < ApplicationController
 
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv
+    end
+
+  end
 
 end
